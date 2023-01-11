dd0ecced6d2d3ee6acbcd35742a452e26f460991
mrtark <murat_ark97@hotmail.com>
2023-01-11 17:46:18 +0300

    ProductDto ref obj + html data send (bootstrapcart)

--- src/main/java/com/example/springboot/springboot/controller/mvc/ProductControllermvc.java
+++ src/main/java/com/example/springboot/springboot/controller/mvc/ProductControllermvc.java
@@ -1,7 +1,9 @@
 package com.example.springboot.springboot.controller.mvc;
 
+import com.example.springboot.springboot.business.dto.ProductDto;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -33,5 +35,25 @@ public class ProductControllermvc {
     //@ResponseBody html den veri alacağımız için bu yapıyı kapattık
     public String mvc2(){
         return "data2page";
+    }
+
+    //http://localhost:8080/root/data3
+    @GetMapping("data3")
+    public String mvc3(Model refModel){
+        refModel.addAttribute("key_2","Data From Java! :)");
+        return "data3page_sendData";
+    }
+
+    //http://localhost:8080/root/data4
+    @GetMapping("data4")
+    public String mvc4(Model refModel){
+        ProductDto refproductdto = ProductDto
+                .builder()
+                .id(14L)
+                .name("Graphic card")
+                .price("4.000")
+                .build();
+        refModel.addAttribute("product1",refproductdto);
+        return "data4page_sendDataProduct";
     }
 }
