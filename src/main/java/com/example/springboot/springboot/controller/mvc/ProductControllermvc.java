5816a9598a946736da0a4d4661640f049a41e280
mrtark <murat_ark97@hotmail.com>
2023-01-11 15:03:06 +0300

    @GetMapping + @ResponseBody = hello world

--- src/main/java/com/example/springboot/springboot/controller/mvc/ProductControllermvc.java
+++ src/main/java/com/example/springboot/springboot/controller/mvc/ProductControllermvc.java
@@ -1,12 +1,17 @@
 package com.example.springboot.springboot.controller.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("mvc")
+@RequestMapping("root")
 public class ProductControllermvc {
+    //http://localhost:8080/mvc/veri1
+    @GetMapping("data")
+    @ResponseBody
     public String mvc(){
-        return "index.html";
+        return "Hello World";
     }
 }
