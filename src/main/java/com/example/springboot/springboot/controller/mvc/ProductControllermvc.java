2dd44dac6740b81739fa31168d4733ab59e703e3
mrtark <murat_ark97@hotmail.com>
2023-01-11 16:36:50 +0300

    pom.xml update + dizin güncellendi + data2page.html np

--- src/main/java/com/example/springboot/springboot/controller/mvc/ProductControllermvc.java
+++ src/main/java/com/example/springboot/springboot/controller/mvc/ProductControllermvc.java
@@ -1,17 +1,37 @@
 package com.example.springboot.springboot.controller.mvc;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.TimeZone;
+
 @Controller
 @RequestMapping("root")
 public class ProductControllermvc {
-    //http://localhost:8080/mvc/veri1
+    //Bean constructer
+    @PostConstruct
+    public void getMyLocalTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+    }
+    //normal constructer
+    public ProductControllermvc() {
+
+    }
+
+    //http://localhost:8080/root/data
     @GetMapping("data")
     @ResponseBody
     public String mvc(){
         return "Hello World";
+    }
+
+    //http://localhost:8080/root/data2
+    @GetMapping("data2")
+    //@ResponseBody html den veri alacağımız için bu yapıyı kapattık
+    public String mvc2(){
+        return "data2page";
     }
 }
