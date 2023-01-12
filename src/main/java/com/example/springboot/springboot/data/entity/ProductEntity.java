9ff86927810d8f3bff882ca15508560f487fe9b6
mrtark <murat_ark97@hotmail.com>
2023-01-12 19:24:58 +0300

    ProductEntity costumize

--- src/main/java/com/example/springboot/springboot/data/entity/ProductEntity.java
+++ src/main/java/com/example/springboot/springboot/data/entity/ProductEntity.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -18,7 +21,14 @@ import lombok.extern.log4j.Log4j2;
 public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id", updatable = false,insertable = true,nullable = false,unique = true)
     private Long id;
+    @Column(name = "product_name", length = 100)
     private String name;
+    @Column(name = "product_price")
     private String price;
+    @Column(name = "created_date")
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
 }
---
+++ src/main/java/com/example/springboot/springboot/data/repository/IProductRepository.java
@@ -0,0 +1,4 @@
+package com.example.springboot.springboot.data.repository;
+
+public interface IProductRepository {
+}
