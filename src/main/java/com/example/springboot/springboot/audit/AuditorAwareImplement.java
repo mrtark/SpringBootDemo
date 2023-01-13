429f0d4faeb67923aa478030a11ca00b657d01c3
mrtark <murat_ark97@hotmail.com>
2023-01-13 17:01:14 +0300

    + AuditorAwareImpl

---
+++ src/main/java/com/example/springboot/springboot/audit/AuditorAwareImplement.java
@@ -0,0 +1,12 @@
+package com.example.springboot.springboot.audit;
+
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+public class AuditorAwareImplement implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("MuratArik");
+    }
+}
