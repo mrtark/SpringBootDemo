fc3a28db7e2838f83b715d2fbf9a94918014656e
mrtark <murat_ark97@hotmail.com>
2023-01-14 14:14:45 +0300

    + @EntityListeners

--- src/main/java/com/example/springboot/springboot/data/entity/BaseEntity.java
+++ src/main/java/com/example/springboot/springboot/data/entity/BaseEntity.java
@@ -8,12 +8,15 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.util.Date;
 
 @Getter
 @Setter
+
+@EntityListeners(AuditingEntityListener.class)
 
 @MappedSuperclass
 public class BaseEntity implements Serializable {
