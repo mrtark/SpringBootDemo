9fd6c67c3ca9c336829c5f747ebe3930a9741239
mrtark <murat_ark97@hotmail.com>
2023-01-14 14:11:39 +0300

    + @EnableJpaAuditing

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -2,8 +2,11 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication()
+
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareBean")
 public class Application {
 
     public static void main(String[] args) {
